0d0a4fbb792b46735dd5e26b6386c0c18098df15
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-01 15:24:20 +0530

    Instance for service using test bed injcet

--- src/app/services/Logger/logger.service.spec.ts
+++ src/app/services/Logger/logger.service.spec.ts
@@ -6,11 +6,45 @@ describe('LoggerService', () => {
   let service: LoggerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LoggerService],
+    });
+
+    //get instance of logger service from TestBed
     service = TestBed.inject(LoggerService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should not have any messages at starting', () => {
+    //arrange
+    //const service = new LoggerService();
+
+    //act
+    let count = service.messages.length;
+
+    //assert
+    expect(count).toBe(0);
   });
+
+  it('should add the message when log is called', () => {
+    //arrange
+    //const service = new LoggerService();
+
+    //act
+    service.log('message');
+
+    //assert
+    expect(service.messages.length).toBe(1);
+  });
+
+  /* it('should clear all the messages when clear is called', () => {
+    //arrange
+    //const service = new LoggerService();
+    service.log('message');
+
+    //act
+    service.clear();
+
+    //assert
+    expect(service.messages.length).toBe(0);
+  }); */
 });
