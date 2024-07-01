f490971ec2c89572ca019b4a91db4becef7c63b1
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-01 13:41:30 +0530

    child coponent  test

--- src/app/componenets/posts/posts.component.spec.ts
+++ src/app/componenets/posts/posts.component.spec.ts
@@ -4,6 +4,9 @@ import { PostsComponent } from './posts.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostService } from '../../services/Post/post.service';
 import { Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 class mockPostService {
   getPosts() {}
@@ -24,7 +27,6 @@ describe('PostsComponent', () => {
   })
   class FakePostComponent {
     @Input() post!: Post;
-    
   }
 
   beforeEach(() => {
@@ -45,18 +47,31 @@ describe('PostsComponent', () => {
         title: 'title3',
       },
     ];
+    const mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: () => 'test', // Mock any route parameters as needed
+        },
+      },}
+
     mockPostService = jasmine.createSpyObj(['getPosts', 'deletePosts']);
+    
 
     TestBed.configureTestingModule({
-      imports: [PostsComponent],
+      imports: [PostsComponent,RouterTestingModule],
 
       providers: [
         {
           provide: PostService,
           useValue: mockPostService,
         },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+
       ],
     });
+
+
+    
 
     fixture = TestBed.createComponent(PostsComponent);
     component = fixture.componentInstance;
@@ -65,8 +80,15 @@ describe('PostsComponent', () => {
   it('Should set posts from the service directly', () => {
     mockPostService.getPosts.and.returnValue(of(POSTS));
     component.ngOnInit(); //fixture.detectChanges(); can use
-
     expect(component.posts.length).toBe(3);
+  });
+
+  it('Should create one post child element for each post', () => {
+    mockPostService.getPosts.and.returnValue(of(POSTS));
+    fixture.detectChanges(); 
+    const debugElement = fixture.debugElement;
+    const postsElment = debugElement.queryAll(By.css('.posts'));
+    expect(postsElment.length).toBe(POSTS.length);
   });
 
   //Delete method eka test tika 3 scenarrios
