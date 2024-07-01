90c4a6b48295c450a3845913d856a86446074d13
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-01 08:13:58 +0530

    TestBed Initiated for post component

--- src/app/componenets/post/post.component.spec.ts
+++ src/app/componenets/post/post.component.spec.ts
@@ -1,15 +1,31 @@
 import { PostComponent } from './post.component';
 import { Post } from '../../models/post';
 import { first } from 'rxjs';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 
 describe('Post Component', () => {
+  let fixture: ComponentFixture<PostComponent>;
+  let comp: PostComponent;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PostComponent],
+    });
+
+    fixture = TestBed.createComponent(PostComponent);
+    comp = fixture.componentInstance;
+  });
+
+  it('should create post component Using TestBed', () => {
+    expect(comp).toBeDefined();
+  });
+
   it('should raise and event when the delete post is clicked', () => {
-    const comp = new PostComponent();
     const post: Post = { id: 1, title: 'testTile1', body: 'testBody1' };
     comp.post = post;
 
     comp.deletePost.pipe(first()).subscribe((slectedPost) => {
-        expect(slectedPost).toEqual(post);
+      expect(slectedPost).toEqual(post);
     });
 
     comp.onDeletePost(new MouseEvent('click'));
