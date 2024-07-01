08f1dcfdf3775cd08dff0a98e7e77af1ca0f6e83
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-01 10:19:01 +0530

    Test Bed in HTML tag

--- src/app/componenets/post/post.component.spec.ts
+++ src/app/componenets/post/post.component.spec.ts
@@ -1,7 +1,8 @@
 import { PostComponent } from './post.component';
 import { Post } from '../../models/post';
-import { first } from 'rxjs';
+import { first, of } from 'rxjs';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 
 describe('Post Component', () => {
@@ -10,6 +11,14 @@ describe('Post Component', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [PostComponent],
+      providers:[
+        {
+          provide:ActivatedRoute,
+          useValue:{
+            params:of({id: 'testID'})
+          }
+        }
+      ]
     });
 
     fixture = TestBed.createComponent(PostComponent);
@@ -19,6 +28,19 @@ describe('Post Component', () => {
   it('should create post component Using TestBed', () => {
     expect(comp).toBeDefined();
   });
+
+
+  //Check html elemnts 
+  it('Should render the title in <a> tag', () => {
+
+    const post: Post = { id: 1, title: 'Title01', body: 'testBody1' };
+    comp.post = post;
+    fixture.detectChanges();
+    const postElement: HTMLElement = fixture.nativeElement;
+    const a = postElement.querySelector('a');
+    expect(a?.textContent).toEqual(post.title);
+
+  })
 
   it('should raise and event when the delete post is clicked', () => {
     const post: Post = { id: 1, title: 'testTile1', body: 'testBody1' };
