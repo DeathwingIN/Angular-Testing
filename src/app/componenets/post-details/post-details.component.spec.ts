8453f81cbc82c5253d1c770f14549104da867024
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-05 12:08:07 +0530

    41

--- src/app/componenets/post-details/post-details.component.spec.ts
+++ src/app/componenets/post-details/post-details.component.spec.ts
@@ -1,23 +1,55 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostDetailsComponent } from './post-details.component';
+import { CommonModule, Location } from '@angular/common';
+import { PostService } from '../../services/Post/post.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Post } from '../../models/post';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 describe('PostDetailsComponent', () => {
-  let component: PostDetailsComponent;
   let fixture: ComponentFixture<PostDetailsComponent>;
+  let mockPostService: jasmine.SpyObj<PostService>;
+  beforeEach(() => {
+    let mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: () => {
+            return '3';
+          },
+        },
+      },
+    };
+    mockPostService = jasmine.createSpyObj('PostService', [
+      'getPost',
+      'updatePost',
+    ]);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PostDetailsComponent]
-    })
-    .compileComponents();
+    let mockLocation = jasmine.createSpyObj('Location', ['back']);
+
+    TestBed.configureTestingModule({
+      declarations: [PostDetailsComponent], // Declare the component under test
+      imports: [CommonModule,FormsModule], // Ensure CommonModule is imported here
+      providers: [
+        { provide: Location, useValue: mockLocation },
+        { provide: PostService, useValue: mockPostService },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
+    });
 
     fixture = TestBed.createComponent(PostDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render post tile', () => {
+    mockPostService.getPost.and.returnValue(
+      of({ id: 3, title: 'test title', body: 'Body ' } as Post)
+    );
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('h2')).nativeElement as HTMLElement;
+    expect(element.textContent).toBe(fixture.componentInstance.post.title);
   });
 });
+ */
