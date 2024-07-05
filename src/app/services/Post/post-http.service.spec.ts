dadc013441fbc8f237ec8ba45be563d4b675bdc4
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-05 10:02:18 +0530

    getPost with after each

--- src/app/services/Post/post-http.service.spec.ts
+++ src/app/services/Post/post-http.service.spec.ts
@@ -37,14 +37,34 @@ describe('Http Url Test', () => {
   });
 
   describe('getPosts', () => {
-    it('Should return posts when getPosts() is called', (done:DoneFn) => {
-      postService.getPosts().subscribe((data)=>{
+    it('Should return posts when getPosts() is called', (done: DoneFn) => {
+      postService.getPosts().subscribe((data) => {
         expect(data).toEqual(POSTS);
         done();
       });
-      const request = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/posts')
+      const request = httpTestingController.expectOne(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
       request.flush(POSTS);
       expect(request.request.method).toBe('GET');
     });
+  });
+
+  describe('getPost()', () => {
+    it('Should return post when getPost() is called', (done: DoneFn) => {
+      postService.getPost(1).subscribe((data) => {
+        expect(data).toEqual(POSTS[0]);
+        done();
+      });
+      const request = httpTestingController.expectOne(
+        'https://jsonplaceholder.typicode.com/posts/1'
+      );
+      request.flush(POSTS[0]);
+      expect(request.request.method).toBe('GET');
+    });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
--- src/app/services/Post/post.service.ts
+++ src/app/services/Post/post.service.ts
@@ -3,10 +3,9 @@ import { Post } from '../../models/post';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
-
   constructor(private http: HttpClient) {}
   getPosts() {
     return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
@@ -15,6 +14,12 @@ export class PostService {
   deletePosts(post: Post) {
     return this.http.delete(
       'https://jsonplaceholder.typicode.com/posts/' + post.id
+    );
+  }
+
+  getPost(postId: number) {
+    return this.http.get<Post>(
+      'https://jsonplaceholder.typicode.com/posts/' + postId
     );
   }
 }
